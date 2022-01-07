9b521e235a364c0ae3a31948173795ec5adde98e
hakifred <hakifred20@gmail.com>
2022-01-07 19:32:39 +0300

    windows support added

--- app.py
+++ app.py
@@ -4,9 +4,14 @@ import json
 import csv
 
 def main():
-    if not platform.system().startswith('Linux'):
-        pass
-    bookmarks_path = f"{os.path.expanduser('~')}/.config/google-chrome/Default/Bookmarks"
+    
+    if platform.system().startswith('Linux'):
+        bookmarks_path = f"{os.path.expanduser('~')}/.config/google-chrome/Default/Bookmarks"
+    elif platform.system().startswith('Windows'):
+        user_path = os.path.expanduser('~')
+        bookmarks_path = user_path + r'''\AppData\Local\Google\Chrome\User Data\Default\Bookmarks'''
+    else:
+        print("system not supported")
 
     bookmarks = []
     if os.path.exists(bookmarks_path):
@@ -14,7 +19,10 @@ def main():
             bookmarks_data = json.load(f)
             for data in bookmarks_data["roots"]["bookmark_bar"]["children"]:
                 # book_mark object
-                bookmark = {'Title': data['name'], 'URL': data['url']}
+                try:
+                    bookmark = {'Title': data['name'], 'URL': data['url']}
+                except Exception as error:
+                    data['url'] = 0
                 if bookmark not in bookmarks:
                     bookmarks.append(bookmark)
     else:
